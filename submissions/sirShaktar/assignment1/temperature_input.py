5fc5fd9eaea39acf05e460e6dfe74851219a5e65
gitmacruuf <nimcaan159@gmail.com>
2026-04-22 11:16:06 +0300

    Add assignment1 quiz

---
+++ submissions/sirShaktar/assignment1/temperature_input.py
@@ -0,0 +1,8 @@
+temperature = int(input("Enter temperature: "))
+
+if temperature >= 25:
+    print("warm")
+elif temperature >= 15:
+    print("ok")
+else:
+    print("cold")
--- submissions/temperature_input.py
+++
@@ -1,8 +0,0 @@
-temperature = int(input("Enter temperature: "))
-
-if temperature >= 25:
-    print("warm")
-elif temperature >= 15:
-    print("ok")
-else:
-    print("cold")
